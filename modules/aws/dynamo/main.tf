fd3a52bc447bfc915e5869deb06ac82bb5c156b1
phisco <pscorso93@gmail.com>
2019-09-06 14:23:34 +0200

    fixing iam_user

--- modules/aws/dynamo/main.tf
+++ modules/aws/dynamo/main.tf
@@ -69,5 +69,5 @@ resource "aws_dynamodb_table" "vault-backend" {
 }
 
 resource "aws_iam_access_key" "vault_dynamodb_backend" {
-  user = aws_iam_user.kms_k8s_vault.name
+  user = aws_iam_user.vault_dynamodb_backend.name
 }
